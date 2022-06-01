5c56d33f34858f5378246845aacbd4d3969c07c7
U2yyy <a1535887812@gmail.com>
2022-06-01 10:36:28 +0800

    add insert and foreach

--- copyone.c
+++ copyone.c
@@ -3,7 +3,7 @@
 //
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 struct dynamicArray{
     void ** pArr;
     int ele_size;
@@ -17,8 +17,51 @@ struct dynamicArray *initdynamicArray(int capacity){
     Array->pArr = malloc(sizeof(void*)*capacity);
 }
 
+void insert_dynamicArray(struct dynamicArray* array,int pos,void *date){
+    if(pos<0||pos>array->ele_size)
+        pos = array->ele_size;
+    if(array->ele_size == array->ele_capacity){
+        int newCapacity = array->ele_capacity * 2;
+        void ** newSpace = malloc(sizeof(void*)*newCapacity);
+        memcpy(newSpace,array->pArr, sizeof(void*)*array->ele_capacity);
+        free(array->pArr);
+        array->pArr = newSpace;
+        array->ele_capacity = newCapacity;
+    }
+    for(int i=array->ele_size;i>=pos;i--){
+        array->pArr[i+1] = array->pArr[i];
+    }
+    array->pArr[pos] = date;
+    array->ele_size++;
+}
+
+struct date_structure{
+    int date;
+    char name[99];
+    char hard[99];
+};
+
+void myPrintdate(void* date){
+    struct date_structure *p = date;
+    printf("date is %d  name is %s  hard is %s\n",p->date,p->name,p->hard);
+}
+
+void foreach_dynamicArray(struct dynamicArray* array,void(*myPrint)(void*)){
+    for(int i=0;i<array->ele_size;i++){
+        myPrint(array->pArr[i]);
+    }
+}
+
 int main(){
     struct dynamicArray *Array = initdynamicArray(5);
-    printf("%d%d",Array->ele_size,Array->ele_capacity);
+    printf("%d%d\n",Array->ele_size,Array->ele_capacity);
+    struct date_structure date1 = {1,"dynamicArray","easy"};
+    struct date_structure date2 = {2,"linklist","middle-level"};
+    struct date_structure date3 = {3,"tree","hard"};
+    insert_dynamicArray(Array,-1,&date1);
+    insert_dynamicArray(Array,0,&date2);
+    insert_dynamicArray(Array,3,&date3);
+    foreach_dynamicArray(Array,myPrintdate);
+    printf("%d%d\n",Array->ele_size,Array->ele_capacity);
     return 0;
 }
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 //定义该动态数组
 struct dynamicArray{
     void ** pArr;//这是一个指向数组的指针，负责维护
@@ -19,8 +20,65 @@ struct dynamicArray *init_dynamicArray(int capacity){
     Array->arr_capacity = capacity;
     Array->arr_size = 0;
 }
+//插入数组元素
+void insert_dynamicArray(struct dynamicArray *array,int pos,void* date){
+    //先针对非法的位置请求做处理，这里采用变为尾插的方法
+    if(pos<0||pos>array->arr_size)
+        pos = array->arr_size;
+    //判断是否需要进行动态扩展
+    if(pos == array->arr_capacity){
+        //1.计算新空间大小
+        int newCapacity = array->arr_capacity * 2;
+        //2.分配新的内存空间
+        void ** newSpace = malloc(sizeof(void*)*newCapacity);
+        //3.拷贝
+        memcpy(newSpace,array->pArr, sizeof(void*)*array->arr_capacity);
+        //4.释放原内存空间
+        free(array->pArr);
+        //5.让数组指针指向正确的地方
+        array->pArr = newSpace;
+        //6.更新容量
+        array->arr_capacity =newCapacity;
+    }
+    //进行数据插入
+        //先把原来的数据进行右移
+    for(int i=array->arr_size;i>=pos;i--){
+        array->pArr[i+1] = array->pArr[i];
+    }
+        //进行数据的插入
+    array->pArr[pos] = date;
+    //更新数组元素个数
+    array->arr_size++;
+}
+//定义一个结构体，例如成绩单
+struct Studentsore{
+    char name[99];
+    int score;
+};
+//在这里定义一下我的回调函数
+void myPrintscore(void *date){
+    struct Studentsore *p = date;
+    printf("name:%s  score:%d\n",p->name,p->score);
+}
+//数组元素遍历,这里使用了回调函数，让用户决定传入数据的类型
+void foreach_dynamicArray(struct dynamicArray *array,void(* myPrint)(void*)){
+    for(int i=0;i<array->arr_size;i++){
+        myPrint(array->pArr[i]);
+    }
+}
 int main() {
+    int n;
     struct dynamicArray *Array = init_dynamicArray(5);
     printf("size = %d\ncapacity = %d\n",Array->arr_size,Array->arr_capacity);
+    //数据处理
+    struct Studentsore stu1 = {"yme",100};
+    struct Studentsore stu2 = {"ljd",99};
+    struct Studentsore stu3 = {"MC",10};
+    //将数据插入
+    insert_dynamicArray(Array,-1,&stu1);
+    insert_dynamicArray(Array,0,&stu2);
+    insert_dynamicArray(Array,4,&stu3);
+    //遍历数据
+    foreach_dynamicArray(Array,myPrintscore);
     return 0;
 }
