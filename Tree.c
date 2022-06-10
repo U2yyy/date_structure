eeca3a5aada8aa9bfb2c02e0325fd77495b10795
U2yyy <a1535887812@gmail.com>
2022-06-10 12:15:37 +0800

    添加了copy和free操作,树的递归思想还需要进一步加深理解，我怕我不能 独立写出这些代码来

--- Tree.c
+++ Tree.c
@@ -3,12 +3,13 @@
 //
 //创建树结点的结构体
 #include <stdio.h>
+#include <stdlib.h>
 struct TreeNode{
     char Cha;
     struct TreeNode *lChild;
     struct TreeNode *rChild;
 };
-//树结点占用的数据很少，不用初始化在堆上开辟内存
+//在本次代码实现中树结点占用的数据很少，不用初始化在堆上开辟内存
 //遍历函数
 void foreach_Tree(struct TreeNode *root){
     if(root == NULL)
@@ -35,6 +36,31 @@ int TreeHeight(struct TreeNode *root){
     int rHeight = TreeHeight(root->rChild);
     int height = lHeight > rHeight ? lHeight : rHeight;
     return height+1;
+}
+//拷贝树
+struct TreeNode *copyTree(struct TreeNode *root){
+    if(root == NULL)
+        return NULL;
+    //为每个结点在堆上开辟内存
+    struct TreeNode *newNode = malloc(sizeof(struct TreeNode));
+    //先拷贝左子树
+    newNode->lChild = copyTree(root->lChild);
+    //再拷贝右子树
+    newNode->rChild = copyTree(root->rChild);
+    newNode->Cha = root->Cha;
+    //这里返回的newNode实际上就是newroot
+    return newNode;
+}
+//释放树
+void freeTree(struct TreeNode *root){
+    if(root == NULL)
+        return;
+    //先free左子树
+    freeTree(root->lChild);
+    //再free右子树
+    freeTree(root->rChild);
+    //执行free操作
+    free(root);
 }
 //创建一个测试函数
 void test1(){
@@ -68,7 +94,11 @@ void test1(){
     printf("Amounts of leaves: %d\n",num);
     //求树的深度
     int height = TreeHeight(&nodeA);
-    printf("The height of the tree is: %d",height);
+    printf("The height of the tree is: %d\n",height);
+    //拷贝树
+    struct TreeNode *newTree = copyTree(&nodeA);
+    //遍历拷贝的树
+    foreach_Tree(newTree);
 }
 int main(){
     test1();
