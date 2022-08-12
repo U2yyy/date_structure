86dd8e5fa0613d5fb2691964a748010e9605b498
U2y <a1535887812@gmail.com>
2022-08-12 14:45:38 +0800

    最大堆的基本操作

---
+++ Heap/MaxHeap.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+#define MaxWeight 1000
+
+struct MyHeap{
+	int *weight;
+	int size;
+	int capacity;
+};
+
+typedef struct MyHeap *MaxHeap;
+
+MaxHeap init_MaxHeap(int MaxSize);//初始化最大堆 
+void Insert_MaxHeap(MaxHeap H,int MyWeight);//插入
+int Delete_MaxHeap(MaxHeap H);//删除 
+
+int main(){
+	
+}
+
+MaxHeap init_MaxHeap(int MaxSize){
+	MaxHeap H = malloc(sizeof(struct MyHeap));
+	H->weight = malloc(sizeof(int)*(MaxSize+1));
+	H->weight[0] = MaxWeight;
+	H->size = 0;
+	H->capacity = MaxSize;
+	return H;
+}
+
+void Insert_MaxHeap(MaxHeap H,int MyWeight){
+	int i;
+	i = ++H->size;
+	//这里还很有必要写一个判断堆是否已满的条件表达式 
+	for( ;MyWeight > H->weight[i/2];i /= 2){
+		H->weight[i] = H->weight[i/2]; 
+	}
+	H->weight[i] = MyWeight;
+}
+
+int Delete_MaxHeap(MaxHeap H){
+	int MaxItem = H->weight[1];
+	int temp = MaxHeap[H->size--];
+	int Parent,Child;
+	for(Parent = 1;Parent*2 <= H->size;Parent = Child){
+		Child = 2*Parent;
+		if(Child != H->size&&H->weight[Child]<H->weight[Child+1])
+			Child++;
+		if(H->weight[Child]<temp)
+			break;
+		else
+			H->weight[Parent] = H->weight[Child];
+	}
+	H->weight[Parent] = temp;
+	rerurn MaxItem;
+}
