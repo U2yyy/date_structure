c18f1ef5a969e2706ec5c83a355b86ad7ebda631
u2yyy <a1535887812@gmail.com>
2022-09-23 17:53:33 +0800

    修改了快排的一个bug

--- sort/quickSort(1).c
+++ sort/quickSort(1).c
@@ -1,25 +1,20 @@
 #include<stdio.h>
 
-void Swap(int* a,int* b);
 int median3(int A[],int left,int right);
+void Swap(int *a,int *b);
 void qSort(int A[],int left,int right);
 void quickSort(int A[],int N);
 
 int main(){
-    int a[] = {-99,-3,-999,-2212,1,3,55,9,12,3,4,90,0,12,32};
+    int a[] = {-99,1,3,55,9,12,3,4,90,0,12,32};
     const int length = sizeof(a)/sizeof(a[0]);
     quickSort(a,length);
     for(int i=0;i<length;i++){
         printf("%d ",a[i]);
     }
-    return 0;
+    return 0 ;
 }
 
-void Swap(int* a,int* b){
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
 int median3(int A[],int left,int right){
     int center = (left+right)/2;
     if(A[left]>A[center])
@@ -31,27 +26,31 @@ int median3(int A[],int left,int right){
     Swap(&A[center],&A[right-1]);
     return A[right-1];
 }
+
+void Swap(int *a,int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 void qSort(int A[],int left,int right){
-    /* 
-    如果不想使用限流的做法的话，可以设置这样一个返回条件
-    当然，100以下的数据用别的排序效率会更好
-     */
-    if(right-left<=1)
-        return;
-    int pivot = median3(A,left,right);
-    int i = left;
-    int j = right-1;
-    for(;;){
-        while(A[++i]<pivot){}
-        while(A[--j]>pivot){}
-        if(i<j)
-            Swap(&A[i],&A[j]);
-        else
-            break;
+    if(right>left){
+        int pivot = median3(A,left,right);
+        int i = left;
+        int j = right-1;
+        while(1){
+            while(A[++i]<pivot){}
+            while(A[--j]>pivot){}
+            if(i<j){
+                Swap(&A[i],&A[j]);
+            }else{
+                break;
+            }
+        }
+        Swap(&A[i],&A[right-1]);
+        qSort(A,left,i-1);
+        qSort(A,i+1,right);
     }
-    Swap(&A[i],&A[right-1]);
-    qSort(A,left,i);
-    qSort(A,i+1,right);
 }
 
 void quickSort(int A[],int N){
